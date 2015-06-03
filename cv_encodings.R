8bfa0db98bdf21cc4dae46fb142973f690254a77
michbur <michalburdukiewicz@gmail.com>
2015-06-04 01:24:29 +0200

    too late, I'm giving up

--- cv_encodings.R
+++ cv_encodings.R
@@ -9,11 +9,16 @@ library(randomForest)
 pos_data <- all_seqs[as.logical(targets), ]
 neg_data <- all_seqs[!as.logical(targets), ]
 
+seq_lengths <- apply(pos_data, 1, function(i) length(i) - sum(is.na(i)))
+pos_data6 <- which(sapply(seq_pos, length) == 6)
+pos_datan6 <- which(sapply(seq_pos, length) != 6)
+
 
 coded_pos <- lapply(aa_groups[1L:2], function(single_group) {
-  bitrigrams <- as.matrix(count_multigrams(seq = degenerate(as.matrix(pos_data), single_group), 
-                   ns = c(1, rep(2, 4), rep(3, 3)), a()[-1], 
-                   ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0))))
+  bitrigrams <- as.matrix(count_multigrams(ns = c(1, rep(2, 4), rep(3, 3)), 
+                                           ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0)),
+                                           seq = degenerate(as.matrix(pos_data), single_group),
+                                           u = as.character(1L:length(single_group))))
   
   bitrigrams <- bitrigrams > 0
   storage.mode(bitrigrams) <- "integer"
@@ -23,10 +28,10 @@ coded_pos <- lapply(aa_groups[1L:2], function(single_group) {
 
   
 coded_neg <- lapply(aa_groups[1L:2], function(single_group) {
-  bitrigrams <- as.matrix(count_multigrams(seq = degenerate(as.matrix(neg_data), single_group), 
-                                           ns = c(1, rep(2, 4), rep(3, 3)), a()[-1], 
+  bitrigrams <- as.matrix(count_multigrams(ns = c(1, rep(2, 4), rep(3, 3)), 
                                            ds = list(0, 0, 1, 2, 3, c(0, 0), c(0, 1), c(1, 0)),
-                                           u = 1L:length(single_group)))
+                                           seq = degenerate(as.matrix(neg_data), single_group),
+                                           u = as.character(1L:length(single_group))))
   
   bitrigrams <- bitrigrams > 0
   storage.mode(bitrigrams) <- "integer"
@@ -34,8 +39,6 @@ coded_neg <- lapply(aa_groups[1L:2], function(single_group) {
   bitrigrams
 })
   
-
-
 
 fold_res <- pblapply(1L:50, function(dummy) {
   pos_ids <- cvFolds(sum(targets == 1), K = 5)
@@ -53,11 +56,14 @@ fold_res <- pblapply(1L:50, function(dummy) {
       imp_bigrams <- cut(test_bis, breaks = c(0, 0.05, 1))[[1]]
       
       
-      model_cv <- randomForest(x = rbind(train_pos, train_neg)[, imp_bigrams])
+      model_cv <- randomForest(x = rbind(train_pos, train_neg)[, imp_bigrams], 
+                               as.factor(c(rep(1, sum(pos_ids[[5]] != fold)), rep(0, sum(neg_ids[[5]] != fold)))))
       
-      preds <- cbind(predict(model_cv, rbind(test_pos, test_neg)), c(rep(1, sum(pos_ids[[5]] == fold)),
-                                                                     rep(0, sum(neg_ids[[5]] == fold))))
+      preds <- cbind(as.numeric(as.character(predict(model_cv, rbind(test_pos, test_neg)))), 
+                     c(rep(1, sum(pos_ids[[5]] == fold)), rep(0, sum(neg_ids[[5]] == fold))))
       preds
     })
   })
 })
+
+save(fold_res, file = "amyloid_fold_res")
--- read_data.R
+++ read_data.R
@@ -11,7 +11,7 @@ all_seqs <- t(sapply(c(seq_pos, seq_neg), function(i)
 #lengths of all sequences
 seq_lengths <- ncol(all_seqs) - apply(all_seqs, 1, function(i) sum(is.na(i)))
 
-#remove too short sequence
+#remove too short sequences
 all_seqs <- all_seqs[seq_lengths > 4, ]
 
 targets <- c(rep(1, length(seq_pos) -1), rep(0, length(seq_neg)))
