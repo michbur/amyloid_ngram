ba01539bf1f62a77641b7aa39184db92fbb29820
michbur <michalburdukiewicz@gmail.com>
2015-07-15 22:01:01 +0200

    added new test groups

--- encoding_distance.R
+++ encoding_distance.R
@@ -6,16 +6,14 @@ library(biogram)
 
 sapply(1L:length(aa_groups), function(i)
   sapply(aa_groups[-i], function(j) {
-    print(c(j, aa_groups[[i]]))
+    print(" ")
+    print("LOOP")
+    print(" ")
+    print(dput(j))
+    print(" ")
+    print(dput(aa_groups[[i]]))
     calc_ed(j, aa_groups[[i]])
     })
   )
 
-a <- structure(list(`3` = c("c", "i", "l", "v"), 
-               `2` = c("f", "h", "m", "r", "w", "y"), 
-               `1` = c("a", "d", "e", "g", "k", "n", "p", "q", "s", "t")), .Names = c("3", "2", "1"))
 
-b <- structure(list(`1` = c("g", "a", "p", "v", "l", "i", "m", "f"), 
-               `2` = c("k", "r", "h"), `3` = c("d", "e"), 
-               `4` = c("s", "t", "c", "n", "q", "y", "w")), .Names = c("1", "2", "3", "4"))
-calc_ed(b, a)
