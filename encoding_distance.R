9c50dec5ab1365401b9149aa6bba6ff827d22f8d
michbur <michalburdukiewicz@gmail.com>
2015-07-16 08:25:17 +0200

    newer problem

--- encoding_distance.R
+++ encoding_distance.R
@@ -13,16 +13,16 @@ sapply(1L:length(aa_groups), function(i)
     print(" ")
     print(dput(aa_groups[[i]]))
     calc_ed(j, aa_groups[[i]])
-    })
-  )
+  })
+)
 
 
 a <- structure(list(`1` = c("g", "a", "p", "v", "l", "i", "m", "f"), 
-               `2` = c("k", "r", "h"), 
-               `3` = c("d", "e"), 
-               `4` = c("s", "t", "c", "n", "q", "y", "w")), .Names = c("1", "2", "3", "4"))
+                    `2` = c("k", "r", "h"), 
+                    `3` = c("d", "e"), 
+                    `4` = c("s", "t", "c", "n", "q", "y", "w")), .Names = c("1", "2", "3", "4"))
 
-b <- structure(list(`2` = c("r", "w", "y"), 
-               `3` = c("c", "f", "i", "l", "m", "v"), 
-               `1` = c("a", "d", "e", "g", "h", "k", "n", "p", "q", "s", "t")), .Names = c("2", "3", "1"))
+b <- structure(list(`3` = c("h", "m", "y"),
+                    `2` = c("c", "f", "i", "l", "v", "w"), 
+                    `1` = c("a", "d", "e", "g", "k", "n", "p", "q", "r", "s", "t")), .Names = c("3", "2", "1"))
 calc_ed(a, b)
