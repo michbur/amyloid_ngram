5fca831090b4715c1ef759269b6f723be4bb6715
michbur <michalburdukiewicz@gmail.com>
2015-07-19 20:25:43 +0200

    distances computed

--- encoding_distance.R
+++ encoding_distance.R
@@ -3,26 +3,12 @@ source("read_data.R")
 source("aa_encodings2.R")
 
 library(biogram)
+library(pbapply)
 
-sapply(1L:length(aa_groups), function(i)
+res <- pbsapply(1L:length(aa_groups), function(i)
   sapply(aa_groups[-i], function(j) {
-    print(" ")
-    print("LOOP")
-    print(" ")
-    print(dput(j))
-    print(" ")
-    print(dput(aa_groups[[i]]))
     calc_ed(j, aa_groups[[i]])
   })
 )
 
-a <- structure(list(`2` = c("r", "w", "y"), 
-                    `1` = c("a", "g", "p", "s", "t"), 
-                    `3` = c("d", "e", "h", "k", "n", "q"), 
-                    `4` = c("c", "f", "i", "l", "m", "v")), .Names = c("2", "1", "3", "4"))
 
-b <- structure(list(`2` = c("r", "w", "y"), 
-                    `3` = c("c", "f", "i", "l", "m", "v"), 
-                    `1` = c("a", "d", "e", "g", "h", "k", "n", "p", "q", "s", "t")), 
-               .Names = c("2", "3", "1"))
-calc_ed(a, b)
