2b1f22fee046e1267847b7c95682de0d4b122cc0
michbur <michalburdukiewicz@gmail.com>
2015-07-26 21:29:35 +0200

    Improved script

--- gcb_abstract_poster/poster_script.R
+++ gcb_abstract_poster/poster_script.R
@@ -10,12 +10,10 @@ all_seqs <- t(sapply(c(seq_pos, seq_neg), function(i)
 
 #lengths of all sequences
 seq_lengths <- data.frame(len = c(lengths(seq_pos), lengths(seq_neg)),
-                          tar = c(rep("pos", length(seq_pos)),
-                                 rep("neg", length(seq_neg))))
+                          tar = c(rep("yes", length(seq_pos)),
+                                 rep("no", length(seq_neg))))
 
 save(seq_lengths, file = "gcb_abstract_poster/poster_data.RData")
 
 
-library(ggplot2)
-ggplot(filter(seq_lengths, len < 50), aes(x = len, fill = tar)) +
-  geom_density(alpha = 0.5)
+
