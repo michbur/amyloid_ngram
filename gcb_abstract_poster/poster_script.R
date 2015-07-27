33becf8bf289cc155c4b40168b32f6fb0e5d274a
michbur <michalburdukiewicz@gmail.com>
2015-07-27 12:30:06 +0200

    added clusterization scheme to poster

--- gcb_abstract_poster/poster_script.R
+++ gcb_abstract_poster/poster_script.R
@@ -1,4 +1,5 @@
 library(seqinr)
+library(dplyr)
 
 seq_pos <- read.fasta("gcb_abstract_poster/amyloid_pos_full.fasta", seqtype = "AA")
 seq_neg <- read.fasta("gcb_abstract_poster/amyloid_neg_full.fasta", seqtype = "AA")
@@ -12,8 +13,23 @@ all_seqs <- t(sapply(c(seq_pos, seq_neg), function(i)
 seq_lengths <- data.frame(len = c(lengths(seq_pos), lengths(seq_neg)),
                           tar = c(rep("yes", length(seq_pos)),
                                  rep("no", length(seq_neg))))
+#sequence, length in the invervals
+seq_inter <- data.frame(len = cut(seq_lengths[["len"]], 
+                                  c(min(seq_lengths[["len"]]), 6, 10, 15, max(seq_lengths[["len"]])), 
+                                  include.lowest = TRUE),
+                        tar = seq_lengths[["tar"]])
 
-save(seq_lengths, file = "gcb_abstract_poster/poster_data.RData")
+#sequence, length in the invervals, crosstable
+seq_inter_ct <- data.frame(xtabs(~ len + tar, seq_inter)) %>% group_by(tar) %>% mutate(prop = Freq/sum(Freq)) %>% ungroup
 
+save(seq_inter_ct, file = "gcb_abstract_poster/poster_data.RData")
 
-
+# Density plot that may be useful
+# seq_lengthsf <- filter(seq_lengths, len < 41)
+# seq_lengthsf[["len"]] <- factor(seq_lengthsf[["len"]], levels = 4L:40)
+# 
+# ggplot(data.frame(table(seq_lengthsf[["len"]], seq_lengthsf[["tar"]])), aes(x = Var1, y = Freq, fill = Var2)) +
+#   geom_bar(alpha = 0.5, position = "dodge", stat = "identity") +
+#   scale_fill_discrete("Amyloid") +
+#   scale_x_discrete("Length") +
+#   scale_y_continuous("Count")
